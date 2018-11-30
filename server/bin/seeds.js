806658d365f9f165e75a29c254e7ce32d2d5cddb
Anthony Ioimo <anthonylioimo@gmail.com>
2018-11-30 10:47:28 +0100

    new seed file

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -19,7 +19,7 @@ const bcryptSalt = 10;
 require('../configs/database')
 
 
-let seedRepos = require('./ironhack_repos.json')
+let seedRepos = require('./ironhack_repos_reduced.json')
 
 let repos = seedRepos.map(repo => {
   return {
@@ -32,20 +32,20 @@ let repos = seedRepos.map(repo => {
 
 
 
-let pullRequests = []
+// let pullRequests = []
 
-for (let i = 0; i < repos.length; i++) {
+// for (let i = 0; i < repos.length; i++) {
 
-  axios.get(`https://api.github.com/orgs/ironhack-labs/${repos[i].name}/pulls`)
-  .then(data => {
-    console.log('DEBUG data:', data)
-  })
+//   axios.get(`https://api.github.com/orgs/ironhack-labs/${repos[i].name}/pulls`)
+//   .then(data => {
+//     console.log('DEBUG data:', data)
+//   })
 
 
 
-}
+// }
 
-console.log("repos",repos)
+// console.log("repos",repos)
 
 
 
@@ -68,37 +68,37 @@ console.log("repos",repos)
 //   })
 
 
-// Repo.deleteMany()
-// .then(() => {
-//   return Repo.create(repos)
-// })
-// .then(reposCreated => {
-//   console.log(`${reposCreated.length} repos created with the following id:`);
-//   console.log(reposCreated.map(r => r._id));
-// })
-// .then(() => {
-//   // Close properly the connection to Mongoose
-//   mongoose.disconnect()
-// })
-// .catch(err => {
-//   mongoose.disconnect()
-//   throw err
-// })
-
-
-let pullRequests = []
-
-repos.forEach(repo=>{
-  axios.get('https://api.github.com/repos/ironhack-labs/'+repo.name+'/pulls')
-    .then(pull => {
-      pullRequests.push(pull)
-    })
-    .catch(err=>{
-      console.log("error with Repos for each loop")
-    })
+Repo.deleteMany()
+.then(() => {
+  return Repo.create(repos)
+})
+.then(reposCreated => {
+  console.log(`${reposCreated.length} repos created with the following id:`);
+  console.log(reposCreated.map(r => r._id));
+})
+.then(() => {
+  // Close properly the connection to Mongoose
+  mongoose.disconnect()
+})
+.catch(err => {
+  mongoose.disconnect()
+  throw err
 })
 
-console.log("pullRequests",pullRequests)
+
+// let pullRequests = []
+
+// repos.forEach(repo=>{
+//   axios.get('https://api.github.com/repos/ironhack-labs/'+repo.name+'/pulls')
+//     .then(pull => {
+//       pullRequests.push(pull)
+//     })
+//     .catch(err=>{
+//       console.log("error with Repos for each loop")
+//     })
+// })
+
+// console.log("pullRequests",pullRequests)
 
 
 
