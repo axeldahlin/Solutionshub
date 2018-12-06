14d3613553dea42743db54cf61c8696669949934
Axel <axdahlin@gmail.com>
2018-12-06 17:35:35 +0100

    detail faiil

--- client/src/components/PullDetail.js
+++ client/src/components/PullDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../api';
-
+import Script from 'react-load-script'
+import {Helmet} from 'react-helmet'
 
 
 
@@ -23,20 +24,57 @@ class PullDetail extends Component {
       .catch(err => {
         console.log("Error at apiGetPull",err)
       })
+      const script = document.createElement('script');
+
+      script.src= "https://gist.github.com/axeldahlin/2de492f5aa0c289ef92678e87cea623e.js"
+  
+      script.async = true;
+
+
+  }
+
+  handleScriptCreate() {
+    this.setState({ scriptLoaded: false })
+  }
+   
+  handleScriptError() {
+    this.setState({ scriptError: true })
+  }
+   
+  handleScriptLoad() {
+    this.setState({ scriptLoaded: true })
   }
 
 
 
   render() {
-    console.log("this.props.user PULL DETAIL",this.props.user)
+
+   
+
+    // document.querySelector('.PullDetail').appendChild(script)
+    
 
     if(this.state.pull) {
       return (
-        <div className="PullDetail" >
+        <div id="test" className="PullDetail" >
+
+          <iframe 
+            src="https://gist.github.com/axeldahlin/54d8327ae49456398bc8f512cd6534e6.js"
+          
+          />
+
+            {/* {this.state.script} */}
            <h1>Pull detail</h1>
            <p>{this.state.pull._id}</p>
            <p>{this.state.pull.repoName}</p>
            <p>{this.state.pull.title}</p>
+
+            <Helmet>
+                <script src="https://gist.github.com/axeldahlin/54d8327ae49456398bc8f512cd6534e6.js"></script>
+               
+            </Helmet>
+  
+
          
         <pre>{JSON.stringify(this.state.pull)}</pre>
         <h1>This is the pull details page</h1>
